fd0401a0c155d7078b688fec1f3f45c520fd632b
James Pritchard <james.edward.pritchard@gmail.com>
2023-02-08 12:04:01 -0500

    static path

--- backend/server.js
+++ backend/server.js
@@ -11,18 +11,6 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
-app.get("*", function (_, res) {
-  res.sendFile(
-    path.join(__dirname, "../frontend/build/index.html"),
-    function (err) {
-      if (err) {
-        res.status(500).send(err);
-      }
-    }
-  );
-});
-
 app.use(function (req, res, next) {
   res.header(
     "Access-Control-Allow-Origin",
@@ -44,5 +32,10 @@ db.once("open", () => {
     );
     console.log(colors.bgBlue("--------------------------------"));
   });
+});
+
+app.use(express.static(path.join(__dirname, "../frontend", "build")));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
 });
 module.exports = app;
