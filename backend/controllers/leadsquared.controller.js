6d882ae8e5bc3ebb07a0ad6afd39fa6c6423178e
James Pritchard <james.edward.pritchard@gmail.com>
2023-02-08 16:21:23 -0500

    typo in lsq controller

--- backend/controllers/leadsquared.controller.js
+++ backend/controllers/leadsquared.controller.js
@@ -2,7 +2,7 @@ const asyncHandler = require("express-async-handler");
 const Leadsquared = require("../models/Leadsquared");
 
 const createLeadsquared = asyncHandler(async (req, res) => {
-  const { companyName, accesskey, secretkey } = req.body;
+  const { companyName, accessKey, secretKey } = req.body;
   const leadsquared = await Leadsquared.create({
     companyName: companyName,
     accessKey: accessKey,
