387e73254f63b06f4d994f3236cde0f404e6ecba
James Pritchard <james.edward.pritchard@gmail.com>
2023-02-08 14:02:52 -0500

    added lsq route to route index

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const userRoutes = require("./user.routes");
+const leadsquaredRoutes = require("./leadsquared.routes");
 
 //sets up user and thought routes
 router.use("/user", userRoutes);
+router.use("/lsq", userRoutes);
 
 module.exports = router;
