ef660569a9339a7a7c3b378d968d0795dc949ebc
James Pritchard <james.edward.pritchard@gmail.com>
2023-02-08 09:58:06 -0500

    vercel arrg

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,10 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://dev-objectives-proof.vercel.app/"
+  ); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
