1b222dc6bae8af1f9897229de7b4024a39548034
James Pritchard <james.edward.pritchard@gmail.com>
2023-02-20 10:16:50 -0500

    changed static file path again

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const routes = require("../routes");
 //main router all routes
 router.use("/api/", routes);
-router.use((req, res) => res.send("Wrong route!"));
+// router.use((req, res) => res.send("Wrong route!"));
 
 module.exports = router;
