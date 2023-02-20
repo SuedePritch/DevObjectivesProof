b16d8f140c7e75ce958106c666b8bfaad4f806fa
James Pritchard <james.edward.pritchard@gmail.com>
2023-02-20 10:14:37 -0500

    changed static file path

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ db.once("open", () => {
 });
 
 app.use(express.static(path.join(__dirname, "../frontend", "build")));
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
 });
 module.exports = app;
